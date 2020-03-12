8ef0d09030e2cfce7ca5fe78d133716425e37a2c
daonez <cyongchoi@gmail.com>
2020-03-12 09:50:14 +0900

    practice of modal with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Modal from "react-modal"
 import './App.css';
 
+const customStyles = {
+  overlay:{
+    color:"grey"
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
+};
+Modal.setAppElement("#root");
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  function openModal(){
+    setModalIsOpen(true);
+  }
+ function closeModal() {
+   setModalIsOpen(false)
+ }
   return (
     <div className="App">
-      
+    <button onClick={openModal}>Open modal</button>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} shouldCloseOnOverlayClick={false} style={customStyles}>
+        <h2>Modal Title</h2>
+        <p>Modal body</p>
+        <div>
+          <button onClick={closeModal}>Close</button>
+        </div>
+      </Modal>
     </div>
   );
 }
